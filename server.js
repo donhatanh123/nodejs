293a56a3958745966e3dcc8d9a7fa8ceacf91fb4
donhatanh123 <44740110+donhatanh123@users.noreply.github.com>
2023-09-22 22:15:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
 app.get('/', function (req, res) {
     return res.send({ error: true, message: 'hello' })
 });
-var dbHost = process.env.DB_HOST || 'localhost';
+var dbHost = 'http://43.198.152.21/';
 var dbPort = 3306;
 var dbUser = 'nodejs';
 var dbPassword = '123456a@';
